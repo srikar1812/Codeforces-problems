4903a68df5f7880ed3e2841139ae5df3d10ed40f
Srikar <59122479+srikar1812@users.noreply.github.com>
2020-07-05 00:29:08 +0530

    Add files via upload

---
+++ boiler plate code.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+/* This header file includes all the header files like <iostream>,<vector>,<algorithm>,<cstdio> etc */
+using namespace std;
+int main()
+{
+	//For almost all problems on codeforces involving multiple test cases 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		
+	}
+	return 0;
+	
+}

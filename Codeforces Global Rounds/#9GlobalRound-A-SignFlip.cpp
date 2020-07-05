14d4742753bc64b34af9bb0156264b7e95214c3b
Srikar <59122479+srikar1812@users.noreply.github.com>
2020-07-05 12:06:08 +0530

    Add files via upload

---
+++ Codeforces Global Rounds/#9GlobalRound-A-SignFlip.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int  t;
+	cin>>t;
+	while(t--)
+	{
+	int n;
+	cin>>n;
+	long long a[n];
+	for(int i=0;i<n;i++)
+	cin>>a[i];
+	for(int j=0;j<n;j++)
+{
+if(j%2==0 )
+{
+if(a[j]<0)
+continue;
+else
+a[j]=-a[j];
+}
+ 
+if(j%2!=0  )
+{
+if(a[j]>0)
+continue;
+else
+a[j]=-a[j];
+}
+}
+
+	
+
+	for(int p=0;p<n;p++)
+cout<<a[p]<<' ';
+cout<<endl;
+
+
+
+	
+	
+	
+	}
+	return 0;
+
+	
+}
